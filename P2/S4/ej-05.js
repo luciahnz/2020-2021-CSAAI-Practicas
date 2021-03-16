05c2da48631c8e1acf11e5fe9b88882c76e40106
luciahnz <l.hernanz.2018@alumnos.urjc.es>
2021-03-16 10:15:22 +0100

    ej 05

---
+++ P2/S4/ej-05.js
@@ -0,0 +1,7 @@
+//-- Leer el párrafo identificado como test
+const test = document.getElementById('test')
+
+//-- Mostrar en la consola el contenido del párrafo
+//-- (es la propiedad innerHTML)
+console.log("Párrafo test leido. Dice:")
+console.log(test.innerHTML)
