ad6b640afc9f3cda1f4cd6ea306656b19731bfad
luciahnz <l.hernanz.2018@alumnos.urjc.es>
2021-03-27 13:37:36 +0100

    ej2

---
+++ P2/L5/Ejercicio2.js
@@ -0,0 +1,21 @@
+console.log("Ejecutnado JS...");
+
+//-- ACTIVAR BOTON
+
+Boton = document.getElementById("Boton")
+Boton.onclick = () => {
+    console.log("HOLI")
+}
+
+
+console.log("Ejecutando JavaScript...");
+
+const test = document.getElementById('test')
+test.onclick = () => {
+    console.log("Click sobre el párrafo...")
+}
+
+Boton1 = document.getElementById("Boton1")
+Boton1.onclick = () => {
+    console.log("¡Click!")
+}
