a2d727186188958a8b4e5b3743f0ad724c1a598d
luciahnz <l.hernanz.2018@alumnos.urjc.es>
2021-04-20 09:37:22 +0200

    Ej 4

--- P3/S6/Ej-03.js
+++ P3/S6/Ej-03.js
@@ -1,28 +1,20 @@
-console.log("Ejecutando JS...");
-
-const canvas = document.getElementById("canvas");
-
-//-- Definir el tamaño del convas
-canvas.width = 200;
-canvas.height = 100;
-
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
 
 ctx.beginPath();
-    //-- Línea horizontal
-    ctx.moveTo(10, 20);
-    ctx.lineTo(100, 20);
+    //-- Definir un rectangulo de dimensiones 100x50,
+    //-- cuya esquina superior izquierda está en (5,5)
+    ctx.rect(5,5, 100, 50);
 
-    //-- Línea horizontal y vertical, unidas
-    ctx.moveTo(10, 80);
-    ctx.lineTo(150,80);
-    ctx.lineTo(150,20);
+    //-- Dibujar
+    ctx.fillStyle = 'red';
 
-    ctx.strokeStyle = 'blue';
     //-- Cambiar el tamaño de la linea del trazo
     ctx.lineWidth = 4;
+
+    //-- Rellenar
+    ctx.fill();
 
     //-- Dibujar el trazo
     ctx.stroke()
