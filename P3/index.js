8399c756c1de44d18a1e7666b5709b478aeddc8b
luciahnz <l.hernanz.2018@alumnos.urjc.es>
2021-04-20 11:03:08 +0200

    prueba

--- P3/index.js
+++ P3/index.js
@@ -1,7 +1,28 @@
 console.log("Ejecutando JS...");
 
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("Escenario");
 
-//-- Definir el tamaño del convas
-canvas.width = 500;
-canvas.height = 300;
+//-- ESCENARIO
+canvas.width = 700;
+canvas.height = 400;
+
+
+//-- PALA IZQ
+const ctx = canvas.getContext("2d");
+ctx.beginPath();
+  ctx.rect(10,5, 10, 50);
+  ctx.fillStyle = 'black';
+  ctx.fill();
+    ctx.stroke();
+ctx.closePath();
+
+//-- PALA DER
+const cty = canvas.getContext("2d");
+cty.beginPath();
+  cty.rect(10,5, 10, 50);
+  cty.fillStyle = 'black';
+  cty.fill();
+    cty.stroke();
+cty.closePath();
+
+this.cty_ini = 180;
